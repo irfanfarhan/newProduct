e559001ae07c899245b6c42db33358493fd8ce51
irfan <irfanlbrce@outlook.com>
2023-01-20 19:36:13 +0530

    commit

--- src/app/portal/components/customer/customer.component.ts
+++ src/app/portal/components/customer/customer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MenuItem } from 'primeng/api';
 import { CustomerService } from '../../services/customers.service';
 
@@ -19,27 +20,37 @@ export class CustomerComponent implements OnInit {
   loading: boolean = true;
   orderHistory: any[] = [];
   kountAction: any[] = [];
-  constructor(private customerService: CustomerService) { }
+  editProfileForm: FormGroup = this.fb.group({});
+  editProfileDialog: boolean = false;
+  constructor(private fb: FormBuilder, private customerService: CustomerService) { }
 
   ngOnInit(): void {
+    this.editProfileForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required]
+    });
     this.pCategory = [
       { name: 'Email' },
       { name: 'Loyalty Card' },
       { name: 'Restaurant' }
     ];
     this.items = [
-      { label: 'Edit Profile', icon:'pi pi-pencil' },
-      { label: 'Subscribe to Listrak', icon:'pi pi-external-link' },
-      { label: 'Reset Password', icon:'pi pi-cog' },
-      { label: 'Delete Profile', icon:'pi pi-trash' }
+      {
+        label: 'Edit Profile', icon: 'pi pi-pencil', command: () => {
+          this.editProfile();
+        }
+      },
+      { label: 'Subscribe to Listrak', icon: 'pi pi-external-link' },
+      { label: 'Reset Password', icon: 'pi pi-cog' },
+      { label: 'Delete Profile', icon: 'pi pi-trash' }
     ]
     this.actions = [
-      { label: 'Edit', icon:'pi pi-pencil'},
-      { label: 'Delete', icon:'pi pi-trash' },
-      { label: 'Transfer balance to this card', icon:'pi pi-credit-card' }
+      { label: 'Edit', icon: 'pi pi-pencil' },
+      { label: 'Delete', icon: 'pi pi-trash' },
+      { label: 'Transfer balance to this card', icon: 'pi pi-credit-card' }
     ]
     this.kountAction = [
-      { label: 'Whitelist', icon:'pi pi-pencil'}
+      { label: 'Whitelist', icon: 'pi pi-pencil' }
     ]
     this.customerService.getStoredCards().subscribe(data => {
       this.storedCards = data;
@@ -53,6 +64,23 @@ export class CustomerComponent implements OnInit {
       this.orderHistory = data;
       this.loading = false;
     });
+  }
+
+  editProfile() {
+    this.editProfileDialog = true;
+  }
+
+  hideDialog() {
+    this.editProfileDialog = false;
+  }
+
+  get form() {
+    return this.editProfileForm?.controls;
+  }
+
+  submit() { 
+    console.log(this.editProfileForm?.getRawValue());
+    this.hideDialog();
   }
 
   onRowSelect(event: any) {
