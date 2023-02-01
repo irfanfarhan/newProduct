da630e0c61262deb2dee95f4cf64e3bf8f414140
irfan <irfanlbrce@outlook.com>
2023-02-01 21:12:09 +0530

    commit

--- src/app/portal/components/transfer/transfer.component.ts
+++ src/app/portal/components/transfer/transfer.component.ts
@@ -1,20 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ConfirmationService, Message } from 'primeng/api';
 
 @Component({
   selector: 'app-transfer',
   templateUrl: './transfer.component.html',
-  styleUrls: ['./transfer.component.scss'],
-  providers: [ConfirmationService]
+  styleUrls: ['./transfer.component.scss']
 })
 export class TransferComponent implements OnInit {
   transferBalanceForm: FormGroup = this.fb.group({});
   multiBalanceTransfer: any[] = [];
-  messageShow: Message[] = [];
   total: any = 12;
-  constructor(private fb: FormBuilder,
-    private confirmationService: ConfirmationService) { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.transferBalanceForm = this.fb.group({
@@ -32,20 +28,7 @@ export class TransferComponent implements OnInit {
   }
 
   delete() {
-    this.confirmationService.confirm({
-      message: 'Are you sure you want to delete?',
-      header: 'Confirm Delete',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.messageShow = [
-          { severity: 'success', summary: 'Success', detail: `Deleted successfully`, life: 1000 }
-        ];
-        setTimeout(() => {
-          this.messageShow = [];
-        }, 3000);
-        this.multiBalanceTransfer.pop();
-        this.total = this.total - 12;
-      }
-    });
+    this.multiBalanceTransfer.pop();
+    this.total = this.total - 12;
   }
 }
