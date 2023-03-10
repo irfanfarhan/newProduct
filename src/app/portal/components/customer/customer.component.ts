8b5c18beb6040d7ecd802d66dc947ab345011136
irfan <irfanlbrce@outlook.com>
2023-03-10 22:38:34 +0530

    commit

--- src/app/portal/components/customer/customer.component.ts
+++ src/app/portal/components/customer/customer.component.ts
@@ -48,7 +48,7 @@ export class CustomerComponent implements OnInit {
 
   errorMessage = (message: any) => {
     this.messageShow = [
-      { severity: 'success', summary: 'Success', detail: message, life: 1000 }
+      { severity: 'error', summary: 'Error', detail: message, life: 1000 }
     ];
     const el: any = document.getElementById('mainId');
     el.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
